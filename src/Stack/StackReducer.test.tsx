d6f09aa28486cfb01a91f59fcd6855e80a1f18f9
YohannDelacroix <yohann.delacroix@hotmail.fr>
2024-12-28 19:52:08 +0100

    Stack reducer tests

--- src/Stack/StackReducer.test.tsx
+++ src/Stack/StackReducer.test.tsx
@@ -1,8 +1,39 @@
 import { useReducer } from "react"
-import { StackReducer } from "./StackReducer"
+import { StackReducer, stackActionType } from "./StackReducer"
 
-describe("TESTING STACK REDUCER", () => {
-    it.skip("should", () => {
 
-    })
-})
+describe("Testing StackReducer", () => {
+    it("should add tokens to the stack on ADD_TO_STACK", () => {
+        const initialState = { stack: [] };
+        const action = { type: stackActionType.ADD_TO_STACK, tokens: ["1", "2"] };
+        const newState = StackReducer(initialState, action);
+        expect(newState).toEqual({ stack: ["1", "2"] });
+    });
+
+    it("should remove the last token from the stack on REMOVE_FROM_STACK", () => {
+        const initialState = { stack: ["1", "2"] };
+        const action = { type: stackActionType.REMOVE_FROM_STACK, tokens: ["2"] };
+        const newState = StackReducer(initialState, action);
+        expect(newState).toEqual({ stack: ["1"] });
+    });
+
+    it("should clear the stack on CLEAR", () => {
+        const initialState = { stack: ["1", "2", "3"] };
+        const action = { type: stackActionType.CLEAR, tokens: [] };
+        const newState = StackReducer(initialState, action);
+        expect(newState).toEqual({ stack: [] });
+    });
+
+    it("should evaluate the stack on EVALUATE", () => {
+        const initialState = { stack: ["2", "+", "3"] };
+        // Mock functions used in evaluate
+        jest.mock("../Calc/CalcUtils", () => ({
+            prepareInfixForCalculation: jest.fn(() => ["2", "+", "3"]),
+            toPostfix: jest.fn(() => ["2", "3", "+"]),
+            evaluatePostfixExpression: jest.fn(() => "5"),
+        }));
+        const action = { type: stackActionType.EVALUATE, tokens: [] };
+        const newState = StackReducer(initialState, action);
+        expect(newState).toEqual({ stack: ["5"] });
+    });
+});
